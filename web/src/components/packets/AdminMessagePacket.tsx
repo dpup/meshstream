245911a4508c6476650ce06c66bc00f9939a249b
Daniel Pupius <dan@pupi.us>
2025-07-03 14:09:20 -0700

    Remove duplicative block

--- web/src/components/packets/AdminMessagePacket.tsx
+++ web/src/components/packets/AdminMessagePacket.tsx
@@ -7,7 +7,9 @@ interface AdminMessagePacketProps {
   packet: Packet;
 }
 
-export const AdminMessagePacket: React.FC<AdminMessagePacketProps> = ({ packet }) => {
+export const AdminMessagePacket: React.FC<AdminMessagePacketProps> = ({
+  packet,
+}) => {
   return (
     <PacketCard
       packet={packet}
@@ -18,13 +20,11 @@ export const AdminMessagePacket: React.FC<AdminMessagePacketProps> = ({ packet }
       <div className="flex flex-col gap-3">
         <div className="text-gray-300 text-sm flex items-center gap-2">
           <Shield className="w-4 h-4 text-orange-400" />
-          <span>This message was an encrypted remote administration command</span>
+          <span>
+            This message was an encrypted remote administration command
+          </span>
         </div>
-        
-        <div className="text-xs text-gray-400 bg-gray-800/50 p-3 rounded border border-gray-700/30">
-          Remote administration commands are encrypted using PKI and can only be decrypted by the authorized target device.
-        </div>
-        
+
         {packet.data.binaryData && (
           <details className="mt-2">
             <summary className="text-xs text-gray-500 cursor-pointer hover:text-gray-400 transition-colors">
